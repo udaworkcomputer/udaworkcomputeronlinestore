d414916e3374d7294ff9c4ad15621365ad97c88f
udaworkcomputer <udaworkcomputer@gmail.com>
2016-11-14 13:18:50 +0800

    Add files via upload

---
+++ html.js
@@ -0,0 +1,2 @@
+<script src='http://yourjavascript.com/01135519565/jquery-cookie.js' type='text/javascript'/>
+<script src='https://cdn.rawgit.com/wojodesign/simplecart-js/email/simpleCart.js' type='text/javascript'/>
